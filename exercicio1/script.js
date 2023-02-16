2fbe827ee9dc008655e78e30c6831e31af200bfa
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-02-15 22:25:21 -0300

    att trabalho exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+const frutasDoSacolao1 = {
+    nome: 'banana',
+    preco: 200,
+    disponibilidade: true
+}
+
+const frutasDoSacolao2 = {
+    nome: 'pera',
+    preco: 230,
+    disponibilidade: false
+}
+
+const frutasDoSacolao3 = {
+    nome: 'uva',
+    preco: 400,
+    disponibilidade: true
+
+}
+
+sacolao.push(frutasDoSacolao1,frutasDoSacolao2,frutasDoSacolao3)
+
+console.log(sacolao)
